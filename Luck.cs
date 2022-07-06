28f7b805ad23138d38f30495020953b5be06f4ad
jadsonmarliere <jds.mdo@gmail.com>
2022-07-06 15:12:26 -0300

    First Commit

---
+++ Luck.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Prototipe.Luck.EF
+{
+    public class Luck
+    {
+        public int Id { get; internal set; }
+        public int numero { get; internal set; }
+
+        public override string ToString()
+        {
+            return $"Luck: {this.Id}, {this.numero}";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+
+namespace Prototipe.Luck.EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------- start luck ef prototipe ---------");
+
+        }
+    }
+}
